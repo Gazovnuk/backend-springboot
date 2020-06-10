60a1edbc32e82faa77340b269cbfd5fa4e93ecee
Alexey Litvinov <aleksei545@mail.ru>
2020-06-10 17:56:03 +0400

    add ResponseEntity for priority

--- src/main/java/ru/dev/litvinov/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/ru/dev/litvinov/tasklist/backendspringboot/controller/PriorityController.java
@@ -1,6 +1,8 @@
 package ru.dev.litvinov.tasklist.backendspringboot.controller;
 
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,8 +30,16 @@ public class PriorityController {
         System.out.println("list" + list);
         return list;
     }
+
     @PostMapping("/add")
-    public Priority add(@RequestBody Priority priority) {
-        return priorityRepository.save(priority);
+    public ResponseEntity<Priority> add(@RequestBody Priority priority) {
+        if (priority.getId() != null && priority.getId() != 0) {
+            return new ResponseEntity("redundant param: id must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 }
