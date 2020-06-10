ff2ec1ee3a2d26d2cbabfd2ba8910ff0ab83c36d
Alexey Litvinov <aleksei545@mail.ru>
2020-06-10 18:28:18 +0400

    add find by id mapping

--- src/main/java/ru/dev/litvinov/tasklist/backendspringboot/controller/CategoryController.java
+++ src/main/java/ru/dev/litvinov/tasklist/backendspringboot/controller/CategoryController.java
@@ -1,16 +1,17 @@
 package ru.dev.litvinov.tasklist.backendspringboot.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.dev.litvinov.tasklist.backendspringboot.entity.Category;
-import ru.dev.litvinov.tasklist.backendspringboot.entity.Priority;
 import ru.dev.litvinov.tasklist.backendspringboot.repo.CategoryRepository;
 
 /**
@@ -45,12 +46,23 @@ public class CategoryController {
     }
     @PutMapping("update")
     public ResponseEntity<Category> update(@RequestBody Category category) {
-        if (category.getId() == null && category.getId() == 0) {
+        if (category.getId() == null || category.getId() == 0) {
             return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
         }
         if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
             return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
         }
         return ResponseEntity.ok(categoryRepository.save(category));
+    }
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category category;
+        try {
+            category = categoryRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            return new ResponseEntity("id = " + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(category);
     }
 }
--- src/main/java/ru/dev/litvinov/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/ru/dev/litvinov/tasklist/backendspringboot/controller/PriorityController.java
@@ -1,14 +1,17 @@
 package ru.dev.litvinov.tasklist.backendspringboot.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.dev.litvinov.tasklist.backendspringboot.entity.Category;
 import ru.dev.litvinov.tasklist.backendspringboot.entity.Priority;
 import ru.dev.litvinov.tasklist.backendspringboot.repo.PriorityRepository;
 
@@ -46,12 +49,27 @@ public class PriorityController {
 
     @PutMapping("update")
     public ResponseEntity<Priority> update(@RequestBody Priority priority) {
-        if (priority.getId() == null && priority.getId() == 0) {
+        if (priority.getId() == null || priority.getId() == 0) {
             return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
         }
         if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
             return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
         }
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
         return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Priority> findById(@PathVariable Long id) {
+        Priority priority;
+        try {
+            priority = priorityRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            return new ResponseEntity("id = " + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priority);
     }
 }
